61092164516a5b781738067f097d80462608e192
JoTindle2 <joseph.tindle@snhu.edu>
2020-11-21 18:38:16 -0700

    Create new application API

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,52 @@
-const fs = require('fs');
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const request = require('request');
+const { render } = require('../../app');
+const apiOptions = { 
+    server: 'http://localhost:3000' 
+} 
 
-/* GET travel view */
-const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', { title: pageTitle, trips });
-   };
+/* internal method to render the travel list */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+
+    if(!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if(!responseBody.length) {
+            message = 'No trips exist in database!';
+        }
+    }
+
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
+};
+
+/* GET travel list view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelLIst calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
+}
 
    module.exports = {
-       travel
+       travelList
    };
