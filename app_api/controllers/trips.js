538f1b02dc894eb13c6ea0c228ce30f584b7f075
JoTindle2 <joseph.tindle@snhu.edu>
2020-12-10 17:28:16 -0700

    Add Authentication to Express Backend

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -48,27 +48,27 @@ const tripsAddTrip = async (req, res) => {
     getUser(req, res,
         (req, res) => {
             Trip
-            .create({
-                code: req.body.code,
-                name: req.body.name,
-                length: req.body.length,
-                start: req.body.start,
-                resort: req.body.resort,
-                perPerson: req.body.perPerson,
-                image: req.body.image,
-                description: req.body.description
-            },
-            (err, trip) => {
-                if (err) {
-                    return res
-                    .status(400) // bad request
-                    .json(err);
-                } else {
-                    return res
-                        .status(201) // created
-                        .json(trip);
-                }
-            });
+                .create({
+                    code: req.body.code,
+                    name: req.body.name,
+                    length: req.body.length,
+                    start: req.body.start,
+                    resort: req.body.resort,
+                    perPerson: req.body.perPerson,
+                    image: req.body.image,
+                    description: req.body.description
+                },
+                    (err, trip) => {
+                        if (err) {
+                            return res
+                            .status(400) // bad request
+                            .json(err);
+                        } else {
+                            return res
+                                .status(201) // created
+                                .json(trip);
+                        }
+                    });
         }
     );
 }
