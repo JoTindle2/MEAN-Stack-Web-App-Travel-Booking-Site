ebbdb165aacdabd7046047a15b2d2911b2876fee
JoTindle2 <joseph.tindle@snhu.edu>
2020-12-14 21:20:00 -0700

    Add Authentication to Angular SPA Frontend

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 import { Trip } from '../models/trip';
 import { User } from '../models/user';
@@ -17,8 +17,13 @@ export class TripDataService {
   
   public addTrip(formData: Trip): Promise<Trip> {
     console.log('Inside TripDataService#addTrip');
+    console.log(formData);
+    const headers = new Headers ({ 
+      'Content-Type': 'application/json', 
+      'Authorization': `Bearer ${localStorage.getItem('travlr-token')}`
+      })
     return this.http
-      .post(this.tripUrl, formData) // pass form data in request body
+      .post(this.tripUrl, formData, {headers: headers}) // pass form data and headers in request body
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
@@ -45,8 +50,12 @@ export class TripDataService {
   public updateTrip(formData: Trip): Promise<Trip> {
     console.log('Inside TripDataService#updateTrip');
     console.log(formData);
+    const headers = new Headers ({ 
+      'Content-Type': 'application/json', 
+      'Authorization': `Bearer ${localStorage.getItem('travlr-token')}`
+      })
     return this.http
-      .put(this.tripUrl + formData.code, formData)
+      .put(this.tripUrl + formData.code, formData, {headers: headers})
       .toPromise()
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
